4a042d15a79bd1189efe28ee7867ec4417270d20
sohag-84 <injamulhaqsohag19977@gmail.com>
2023-02-11 22:26:30 +0600

    appbar and location section is desgined

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,6 +1,9 @@
-import 'package:amazon_clone/providers/user_provider.dart';
+// ignore_for_file: prefer_const_constructors
+
+import 'package:amazon_clone/constants/global_variable.dart';
+import 'package:amazon_clone/features/home/widgets/address_box.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String routeName = "/home";
@@ -14,10 +17,67 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    final user = Provider.of<UserProvider>(context).user;
     return Scaffold(
-      body: Center(
-        child: Text(user.toJson()),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(55.h),
+        child: AppBar(
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: GlobalVariables.appBarGradient,
+            ),
+          ),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Container(
+                  height: 42.h,
+                  margin: EdgeInsets.only(left: 15.w),
+                  child: Material(
+                    elevation: 1,
+                    borderRadius: BorderRadius.circular(7.r),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        prefixIcon: Padding(
+                          padding: EdgeInsets.only(left: 6.w),
+                          child: IconButton(
+                            onPressed: null,
+                            icon: Icon(Icons.search),
+                          ),
+                        ),
+                        fillColor: Colors.white,
+                        filled: true,
+                        contentPadding: EdgeInsets.only(top: 20.h),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(7.r),
+                          borderSide: BorderSide.none,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(7.r),
+                          borderSide:
+                              BorderSide(color: Colors.black38, width: 1.w),
+                        ),
+                        hintText: "Search product",
+                        hintStyle: TextStyle(fontWeight: FontWeight.w500,fontSize: 17.sp,),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                height: 42.h,
+                color: Colors.transparent,
+                margin: EdgeInsets.symmetric(horizontal: 10.w),
+                child: Icon(Icons.mic,color: Colors.black,size: 25.h,),
+              )
+            ],
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          AddressBox()
+        ],
       ),
     );
   }
