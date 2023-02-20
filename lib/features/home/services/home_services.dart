eca12997880d28b08787e5650e5aea9bd8ef8952
sohag-84 <injamulhaqsohag19977@gmail.com>
2023-02-20 15:46:33 +0600

    fetched deal of the day product

--- lib/features/home/services/home_services.dart
+++ lib/features/home/services/home_services.dart
@@ -44,4 +44,38 @@ class HomeServices {
     }
     return productList;
   }
+
+  //for deals of the day
+  Future<Product> fetchDealOfDay({
+    required BuildContext context,
+  }) async {
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
+    Product product = Product(
+      name: '',
+      description: '',
+      quantity: 0,
+      images: [],
+      category: '',
+      price: 0,
+    );
+
+    try {
+      http.Response res =
+          await http.get(Uri.parse('$uri/api/deal-of-day'), headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'x-auth-token': userProvider.user.token,
+      });
+
+      httpErrorHandle(
+        response: res,
+        context: context,
+        onSuccess: () {
+          product = Product.fromJson(res.body);
+        },
+      );
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+    return product;
+  }
 }
--- lib/features/home/widgets/deal_of_day.dart
+++ lib/features/home/widgets/deal_of_day.dart
@@ -1,97 +1,117 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:amazon_clone/common/widgets/loader.dart';
+import 'package:amazon_clone/features/home/services/home_services.dart';
+import 'package:amazon_clone/features/product_details/screens/product_details_screen.dart';
+import 'package:amazon_clone/models/product_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class DealOfDay extends StatelessWidget {
+class DealOfDay extends StatefulWidget {
   const DealOfDay({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          alignment: Alignment.topLeft,
-          padding: EdgeInsets.only(top: 10.h, left: 10.w),
-          child: Text(
-            "Deal of the day",
-            style: TextStyle(
-              fontSize: 18.sp,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-        ),
-        SizedBox(height: 5.h),
-        Image.network(
-          "https://cdn.shopify.com/s/files/1/0070/7032/files/image5_4578a9e6-2eff-4a5a-8d8c-9292252ec848.jpg?v=1620247043",
-          height: 220.h,
-          fit: BoxFit.fitHeight,
-        ),
-        Container(
-          alignment: Alignment.topLeft,
-          padding: EdgeInsets.only(left: 15.w),
-          child: Text(
-            "\$999.0",
-            style: TextStyle(fontSize: 18.sp),
-            overflow: TextOverflow.ellipsis,
-          ),
-        ),
-        Container(
-          alignment: Alignment.topLeft,
-          padding: EdgeInsets.only(left: 15.w, right: 40.w, top: 5.h),
-          child: Text(
-            "Camera",
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
-          ),
-        ),
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Image.network(
-                "https://cdn.shopify.com/s/files/1/0070/7032/files/image5_4578a9e6-2eff-4a5a-8d8c-9292252ec848.jpg?v=1620247043",
-                height: 100.h,
-                width: 100.w,
-                fit: BoxFit.fitWidth,
-              ),
-              SizedBox(width: 2.w),
-              Image.network(
-                "https://cdn.shopify.com/s/files/1/0070/7032/files/image5_4578a9e6-2eff-4a5a-8d8c-9292252ec848.jpg?v=1620247043",
-                height: 100.h,
-                width: 100.w,
-                fit: BoxFit.fitWidth,
-              ),
-              SizedBox(width: 2.w),
-              Image.network(
-                "https://cdn.shopify.com/s/files/1/0070/7032/files/image5_4578a9e6-2eff-4a5a-8d8c-9292252ec848.jpg?v=1620247043",
-                height: 100.h,
-                width: 100.w,
-                fit: BoxFit.fitWidth,
-              ),
-              SizedBox(width: 2.w),
-              Image.network(
-                "https://cdn.shopify.com/s/files/1/0070/7032/files/image5_4578a9e6-2eff-4a5a-8d8c-9292252ec848.jpg?v=1620247043",
-                height: 100.h,
-                width: 100.w,
-                fit: BoxFit.fitWidth,
-              ),
-              SizedBox(width: 2.w),
-            ],
-          ),
-        ),
-        Container(
-          padding: EdgeInsets.symmetric(vertical: 15.h).copyWith(left: 15.w),
-          alignment: Alignment.topLeft,
-          child: Text(
-            "See all deals",
-            style: TextStyle(
-              color: Colors.cyan[800],
-            ),
-          ),
-        )
-      ],
+  State<DealOfDay> createState() => _DealOfDayState();
+}
+
+class _DealOfDayState extends State<DealOfDay> {
+  Product? product;
+  final HomeServices homeServices = HomeServices();
+
+  @override
+  void initState() {
+    super.initState();
+    fetchDealOfDay();
+  }
+
+  void fetchDealOfDay() async {
+    product = await homeServices.fetchDealOfDay(context: context);
+    setState(() {});
+  }
+
+  navigateToDetailScreen() {
+    Navigator.pushNamed(
+      context,
+      ProductDetailsScreen.routeName,
+      arguments: product,
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return product == null
+        ? const Loader()
+        : product!.name.isEmpty
+            ? SizedBox()
+            : GestureDetector(
+                onTap: navigateToDetailScreen,
+                child: Column(
+                  children: [
+                    Container(
+                      alignment: Alignment.topLeft,
+                      padding: EdgeInsets.only(top: 10.h, left: 10.w),
+                      child: Text(
+                        "Deal of the day",
+                        style: TextStyle(
+                          fontSize: 18.sp,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 5.h),
+                    Image.network(
+                      product!.images[0],
+                      height: 220.h,
+                      fit: BoxFit.fitHeight,
+                    ),
+                    Container(
+                      alignment: Alignment.topLeft,
+                      padding: EdgeInsets.only(left: 15.w),
+                      child: Text(
+                        "\$${product!.price}",
+                        style: TextStyle(fontSize: 18.sp),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.topLeft,
+                      padding:
+                          EdgeInsets.only(left: 15.w, right: 40.w, top: 5.h),
+                      child: Text(
+                        product!.name,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: product!.images
+                            .map(
+                              (e) => Image.network(
+                                e,
+                                fit: BoxFit.fitWidth,
+                                width: 100,
+                                height: 100,
+                              ),
+                            )
+                            .toList(),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 15.h)
+                          .copyWith(left: 15.w),
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        "See all deals",
+                        style: TextStyle(
+                          color: Colors.cyan[800],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
   }
 }
